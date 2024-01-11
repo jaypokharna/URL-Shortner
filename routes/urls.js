baf0d268d72218886dd1df868881f95018c8b404
jaypokharna <pokharnajay@gmail.com>
2024-01-12 03:44:31 +0530

    Project Completed

---
+++ routes/urls.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+// mongodb connection
+mongoose.connect("mongodb+srv://jayp:2012@cluster0.ozimw5y.mongodb.net/urldb");
+
+// Define the url schema
+const urlSchema = new Schema({
+    shortId: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    redirectUrl: {
+        type: String,
+        required: true,
+    },
+    visitHistory: [{
+        timestamp: { type: Number }
+    }]
+});
+
+// Exporting the url schema
+module.exports = mongoose.model('url', urlSchema);
