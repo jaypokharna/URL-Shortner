278b75e01fa7e3a656a10c52d467338276c9910f
jaypokharna <pokharnajay@gmail.com>
2024-01-12 13:29:20 +0530

    Changes to host

--- routes/urls.js
+++ routes/urls.js
@@ -2,7 +2,9 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 // mongodb connection
-mongoose.connect("mongodb+srv://jayp:2012@cluster0.ozimw5y.mongodb.net/urldb");
+// mongoose.connect("mongodb+srv://jayp:2012@cluster0.ozimw5y.mongodb.net/urldb");
+mongoose.connect(process.env.MONGO_URL);
+
 
 // Define the url schema
 const urlSchema = new Schema({
